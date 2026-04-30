eb1b54259efb831e47821ff7a19f6801885e9ac3
erickED7 <erickdevprogaming@gmail.com>
2026-04-30 14:48:09 -0300

    add  input classname

--- src/components/Inp.jsx
+++ src/components/Inp.jsx
@@ -1,8 +1,10 @@
-const Inp = ({ placeholder, type }) => {
+const Inp = ({ placeholder, type, className = "" }) => {
 
 
     return (
-        <input placeholder={placeholder} type={type} />
+        <input placeholder={placeholder} type={type} 
+            className={`flex items-center justify-start px-4 py-3 w-full rounded-[5px] border border-red-600 placeholder:text-2xl placeholder:text-gray-400 ${className}`}
+        />
     )
 }
 
--- src/sections/CxForm.jsx
+++ src/sections/CxForm.jsx
@@ -4,28 +4,33 @@ const CxForm = () => {
 
 
     return (
-        <div className="flex justify-center items-center px-10 h-full w-full bg-white">
-            <form className="max-w-full space-y-6 bg-blue-400">
-                <div className="flex flex-col bg-red-500">
-                    <span>CARDHOLDER NAME</span>
-                    <Inp placeholder={""} type={"text"} />
+        <div className="flex justify-center items-center px-8 h-full w-full bg-white">
+            <form className="max-w-full space-y-6 border">
+                <div className="flex flex-col ">
+                    <span className="tracking-[2px] text-mist-800">CARDHOLDER NAME</span>
+                    <Inp placeholder={"e.g. Jane Appleseed"} type={"text"} />
                 </div>
-                <div className="flex flex-col bg-red-500">
-                    <span>CARD NUMBER</span>
-                    <input type="text" />
+                <div className="flex flex-col ">
+                    <span className="tracking-[2px] text-gray-800">CARD NUMBER</span>
+                   <Inp placeholder={"e.g. 1234 5678 9123 0000"} type={""} />
                 </div>
                 <div>
-                    <div className="flex bg-red-500">
-                        <span>EXP. DATE (MM/YY)</span>
-                        <span>cvc</span>
-                    </div>
-                    <div className="flex max-w-full overflow-hidden bg-red-500">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
+                    <div className="flex ">
+                        <div className="">
+                            <span className="tracking-[2px]">EXP. DATE</span>
+                            <Inp placeholder={"mm"} type={"text"} className="border-blue-600! w-20!" />
+                        </div>
+                        <div>
+                            <span>(MM/YY)</span>
+                            <Inp placeholder={"yy"} type={"text"} className="w-20!" />
+                        </div>
+                        <div className="">
+                            <span>cvc</span>
+                            <Inp placeholder={"e.g 123"} type={"text"} />
+                        </div>
                     </div>
                 </div>
-                <button className="w-full bg-[#220930] text-white" type="submit">Confirm</button>
+                <button className="px-4 py-4 w-full bg-[#220930] text-white rounded-[10px] text-[18px]" type="submit">Confirm</button>
             </form>
         </div>
     )
